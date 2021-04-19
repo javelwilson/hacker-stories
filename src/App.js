a91a785dd6fd4ead4317ea35d99fc937ffcbf94a
Javel Wilson <javelawilson@gmail.com>
2021-04-18 22:50:22 -0500

    Lifting state in React

--- src/App.js
+++ src/App.js
@@ -21,9 +21,15 @@ const App = () => {
     },
   ]
 
+  const [searchTerm, setSearchTerm] = useState('')
+
   const handleSearch = (event) => {
-    console.log(event.target.value)
+    setSearchTerm(event.target.value)
   }
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  )
 
   return (
     <div>
@@ -33,7 +39,7 @@ const App = () => {
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   )
 }
