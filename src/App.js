c5d00890f9bdb9898022bf229f2c473c4a71e946
Javel Wilson <javelawilson@gmail.com>
2021-04-18 22:14:28 -0500

    Refactor JSX

--- src/App.js
+++ src/App.js
@@ -19,46 +19,40 @@ const list = [
   },
 ]
 
-const App = () => {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
 
-      <Search />
+    <Search />
 
-      <hr />
+    <hr />
 
-      <List />
-    </div>
-  )
-}
+    <List />
+  </div>
+)
 
-const Search = () => {
-  return (
-    <div>
-      <label htmlFor="search">Search:</label>
-      <input type="text" id="search" />
-    </div>
-  )
-}
+const Search = () => (
+  <div>
+    <label htmlFor="search">Search:</label>
+    <input type="text" id="search" />
+  </div>
+)
 
-const List = () => {
-  return (
-    <ul>
-      {list.map(function (item) {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        )
-      })}
-    </ul>
-  )
-}
+const List = () => (
+  <ul>
+    {list.map(function (item) {
+      return (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      )
+    })}
+  </ul>
+)
 
 export default App
