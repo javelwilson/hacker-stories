f597eb30246c23b593a7e72e8b842b6a49e9612f
Javel Wilson <javelawilson@gmail.com>
2021-04-18 21:52:22 -0500

    React JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+function getTitle(title) {
+  return title
+}
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search:</label>
+      <input type="text" id="search" />
+    </div>
+  )
 }
 
 export default App
