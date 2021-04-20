cf162108c2636a08dae560de5e7facaba5b5ed64
Javel Wilson <javelawilson@gmail.com>
2021-04-20 16:41:33 -0500

    CSS in React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useReducer } from 'react'
 import axios from 'axios'
+import './App.css'
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -96,8 +97,8 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -128,7 +129,11 @@ const SearchForm = ({ searchTerm, onSearchInput, onSearchSubmit }) => (
     >
       <strong>Search:</strong>
     </InputWithLabel>
-    <button type="submit" disabled={!searchTerm}>
+    <button
+      type="submit"
+      disabled={!searchTerm}
+      className="button button_large"
+    >
       Submit
     </button>
   </form>
@@ -153,13 +158,16 @@ const InputWithLabel = ({
   }, [isFocused])
   return (
     <>
-      <label htmlFor={id}>{children}</label>
+      <label htmlFor={id} className="label">
+        {children}
+      </label>
       <input
         type={type}
         id={id}
         onChange={onInputChange}
         value={value}
         ref={inputRef}
+        className="input"
       />
     </>
   )
@@ -181,15 +189,19 @@ const List = ({ list, onRemoveItem }) => {
 const Item = ({ item, onRemoveItem }) => {
   console.log('Item renders')
   return (
-    <li>
-      <span>
+    <li className="item">
+      <span style={{ width: '40%' }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: '30%', color: 'red' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments}</span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
+        <button
+          type="button"
+          className="button button_small"
+          onClick={() => onRemoveItem(item)}
+        >
           Dismiss
         </button>
       </span>
