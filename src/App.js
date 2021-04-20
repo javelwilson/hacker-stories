98a3eff976d5e16568dd6f5ca11d6dbd3f75a578
Javel Wilson <javelawilson@gmail.com>
2021-04-19 20:11:31 -0500

    React advanced stories with useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -36,11 +36,22 @@ const getAsyncStories = () =>
     setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
   )
 
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload
+    case 'REMOVE_STORY':
+      return state.filter((story) => action.payload.objectID !== story.objectID)
+    default:
+      throw new Error()
+  }
+}
+
 const App = () => {
   console.log('App renders')
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React')
-  const [stories, setStories] = useState([])
+  const [stories, dispatchStories] = useReducer(storiesReducer, [])
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
 
@@ -48,17 +59,17 @@ const App = () => {
     setIsLoading(true)
     getAsyncStories()
       .then((result) => {
-        setStories(result.data.stories)
+        dispatchStories({ type: 'SET_STORIES', payload: result.data.stories })
         setIsLoading(false)
       })
       .catch(() => setIsError(true))
   }, [])
 
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    )
-    setStories(newStories)
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    })
   }
 
   const handleSearch = (event) => {
